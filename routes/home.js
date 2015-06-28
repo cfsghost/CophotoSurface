41e158b073ed2c91e232abdd40a8b924460caede
Fred Chien <cfsghost@gmail.com>
2015-06-28 10:14:00 +0800

    support uploading

--- routes/home.js
+++ routes/home.js
@@ -1,4 +1,7 @@
-
+var fs = require('fs');
+var os = require('os');
+var path = require('path');
+var parse = require('co-busboy');
 var Router = require('koa-router');
 
 var router = module.exports = new Router();
@@ -13,4 +16,21 @@ router.post('/', function *() {
 
 router.get('/takephoto', function *() {
     yield this.render('takephoto');
+});
+
+router.post('/uploadphoto', function *() {
+
+	if (!this.request.is('multipart/*'))
+		return yield next;
+
+	var parts = parse(this);
+	var part;
+
+	while(part = yield parts) {
+		var stream = fs.createWriteStream(path.join(os.tmpdir(), Math.random().toString()));
+		part.pipe(stream);
+		console.log('uploading %s -> %s', part.filename, stream.path);
+	}
+
+	this.body = 'DONE';
 });
